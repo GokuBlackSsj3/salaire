71b76307026ec64eb261397fd89bf3964381bb99
GokuBlackSsj3 <sarakoceila@gmail.com>
2024-09-19 16:50:58 +0200

    first commit

---
+++ src/main/java/fr/dampierre/App.java
@@ -0,0 +1,23 @@
+package fr.dampierre;
+
+import java.util.Scanner;
+
+public class App {
+    
+    public static void main(String[] args) {
+        Scanner clavier = new Scanner(System.in);
+
+        System.out.println("Enter votre salaire horaire: ");
+        int salaireHoraire = clavier.nextInt();
+
+        System.out.println("Enter vos heures travaillées: ");
+        int nbHeuresTravaillees = clavier.nextInt();
+
+        int res = salaireHoraire * nbHeuresTravaillees;
+
+        System.out.println("Le salaire brut est :" + res);
+    }
+}
+
+
+
