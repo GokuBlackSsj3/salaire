3f7c4586085228be08aeab2f20ec506b3d1f31a3
GokuBlackSsj3 <sarakoceila@gmail.com>
2024-10-04 14:47:10 +0200

    exercice5

---
+++ src/main/java/fr/dampierre/exercice5.java
@@ -0,0 +1,15 @@
+public static void main(String[] args) {
+    Random number = new Random();
+    int numberToGuess = number.nextInt(100) + 1;
+    int numberOfTries = 0;
+    Scanner input = new Scanner(System.in);
+    System.out.println("Je pense à un nombre entre 1 et 100 . Devinez lequel.");
+}
+    while (true) {
+        System.out.print("Entrez un nombre : ");
+        int guess = input.nextInt();
+        numberOfTries++;
+}
+    
+
+
---
+++ src/main/java/fr/dampierre/pypasdethon.py
@@ -0,0 +1,6 @@
+a=float (input("saisir la valeur de a"))
+res=a+1
+res=res*res
+res=res-2*a-1
+print (res)
+
